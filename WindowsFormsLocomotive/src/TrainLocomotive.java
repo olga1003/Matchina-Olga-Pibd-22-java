11268d2ec8f15931fdddfd484dca60ae28f42f04
olga1003 <55150916+olga1003@users.noreply.github.com>
2019-11-06 16:17:13 +0400

    Update TrainLocomotive.java

--- WindowsFormsLocomotive/src/TrainLocomotive.java
+++ WindowsFormsLocomotive/src/TrainLocomotive.java
@@ -2,19 +2,11 @@ import java.awt.Color;
 import java.util.*;
 import java.awt.Graphics;
 import java.lang.Enum;
-
-
 public class TrainLocomotive extends LocoTrain {
-
-	/// Дополнительный цвет
+	
 	public  Color DopColor; { Set get;}
-
-	/// Признак наличия трубы
 	public  boolean Steam; { Set get;}
-
-	/// Признак наличия угля
 	public  boolean Coal;
-
 	public TrainLocomotive(int maxSpeed, float weight, Color mainColor, Color dopColor,
 			boolean steam, boolean coal) {
 		super(maxSpeed, weight, mainColor);
@@ -22,24 +14,14 @@ public class TrainLocomotive extends LocoTrain {
 		this.Steam = steam;
 		this.Coal = coal;
 	}
-
-
-
 	@Override
 	public  void  DrawTrain(Graphics g)
 	{	
-
-
-
-
 		if (Coal)
 		{
 			g.setColor(DopColor);
 			g.fillOval( _startPosX + 77, _startPosY + 35, 10, 7);		
 		}
-
-
-
 		if (Steam)
 		{
 			g.setColor(Color.GRAY);
@@ -48,12 +30,7 @@ public class TrainLocomotive extends LocoTrain {
 			g.fillOval(_startPosX + 36, _startPosY + 5, 9, 7);
 			g.setColor(DopColor);
 			g.fillRect( _startPosX + 25, _startPosY + 25, 10, 15);
-
-
 		}
-
 		super.DrawTrain(g);   
 	}
-
 }
-
