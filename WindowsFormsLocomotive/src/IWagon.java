88caae72dd65d22e7b6ab998d328bd9f7f75ab13
helga5622@gmail.com <helga5622@gmail.com>
2019-12-06 14:22:50 +0400

    Домашняя работа 2

---
+++ WindowsFormsLocomotive/src/IWagon.java
@@ -0,0 +1,4 @@
+import java.awt.Graphics;
+public interface IWagon {
+	void draw(Number number, Graphics g,  int x ,int y);
+}
--- WindowsFormsLocomotive/src/JPanelLocomotive.java
+++ WindowsFormsLocomotive/src/JPanelLocomotive.java
@@ -2,14 +2,14 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class JPanelLocomotive extends JPanel {
-	TrainLocomotive transpotr;
-	public void  drawLocomotive(TrainLocomotive transpotr) {		
-		this.transpotr = transpotr;
+	ITransport transport;
+
+	public JPanelLocomotive(ITransport transpotr) {			
+		this.transport = transpotr;
 	}
 
 	public void paint(Graphics g)  {		
 		super.paint(g);		
-		transpotr.DrawTrain(g);
+		transport.DrawTrain(g);
 	}
 }
-
